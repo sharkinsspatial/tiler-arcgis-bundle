5a14f558f87a44f9b6bdd84b0781735b7e1878d9
FuZhenn <fuzhen@maptalks.org>
2016-03-16 13:15:12 +0800

    update test.js

--- test/test.js
+++ test/test.js
@@ -1,22 +1,14 @@
-var Tiler = require('../index');
-
+var Tiler = require('../index'),
+    fs = require('fs'),
+    assert = require('assert');
+    
 var tiler = new Tiler(__dirname+'/sample');
 
 tiler.getTile(0xd50, 0x971, 2, function(error, tile) {
     if (error) {
         throw error;
-    }
-    if (!tile) {
-        throw new Error('data is null');
-    }
-    if (!tile.lastModified) {
-        throw new Error('lastModified is null');
-    }
-    if (!tile.data) {
-        throw new Error('data is null');
-    }
-    if (!tile.data.length === 0) {
-        throw new Error('data is empty');
-    }
-    console.info('success!');
+    }    
+    assert.ok(tile);   
+    assert.ok(!isNaN(Date.parse(tile.lastModified)));
+    assert.deepEqual(tile.data, fs.readFileSync(__dirname + '/tile.png'));    
 });
